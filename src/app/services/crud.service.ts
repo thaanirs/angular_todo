69b0d16d69e28ea2f4145db3f25e4f95e5adff82
srinaath narasimhan <srinaath2002@gmail.com>
2023-09-02 15:28:03 +0530

    crud service crud ops

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Task } from '../models/task.model';
+import { Observable,of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,4 +10,17 @@ export class CrudService {
   constructor(private http: HttpClient) { 
     this.serviceURL = "http://localhost:3000/tasks"
    }
+  addTask(task:Task):Observable<Task>{
+    return this.http.post<Task>(this.serviceURL,task)
+  }
+  getAllTask(task:Task):Observable<Task[]>{
+    return this.http.get<Task[]>(this.serviceURL)
+  }
+  deleteTask(task:Task):Observable<Task>{
+    return this.http.delete<Task>(this.serviceURL+"/"+task.id)
+  }
+  editTask(task:Task):Observable<Task>{
+    return this.http.put<Task>(this.serviceURL+"/"+task.id,task)
+  }
+  
 }
