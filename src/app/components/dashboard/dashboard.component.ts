95578e56529b437d655f3ca945f7aebfad4b183c
srinaath narasimhan <srinaath2002@gmail.com>
2023-09-03 14:25:06 +0530

    basic todo complete

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
-
+import { CrudService } from 'src/app/services/crud.service';
+import { Task } from 'src/app/models/task.model';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  taskObj: Task = new Task();
+  taskArr: Task[] = [];
+  addTaskValue: string = '';
+  editTaskValue:string='';
+  constructor(private crudService: CrudService) { }
+
+  ngOnInit(): void {
+    this.addTaskValue = ''
+    this.editTaskValue = ''
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTask();
+  }
+  addTask() {
+    this.taskObj.task = this.addTaskValue
+    this.crudService.addTask(this.taskObj).subscribe(
+      data => {this.ngOnInit();this.addTaskValue='';}, 
+      err => {alert(err);}
+    )
+  }
+  editTask() {
+    this.taskObj.task = this.editTaskValue
+    this.crudService.editTask(this.taskObj).subscribe(
+      data => {this.ngOnInit();},
+      err => {alert("Failed to update task")}
+    )
+  }
+  delteTask(task: Task) {
+    this.crudService.deleteTask(task).subscribe(
+      data=>{this.ngOnInit()},
+      err=>{alert("failed to delete")}
+    )
+  }
+  getAllTask() {
+    this.crudService.getAllTask().subscribe(
+      data => {this.taskArr = data;}, 
+      err => {alert("Unable to get list of tasks");}
+    )
+  }
+
+  call(task:Task){
+    this.taskObj = task;
+    this.editTaskValue = task.task
+  }
 
 }
